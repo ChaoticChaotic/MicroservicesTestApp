5b73af72fd80e5a0a4b4f74ded7692865a196ad4
Alexandr <sashajob@bk.ru>
2024-07-12 20:44:33 +0400

    added README, added swagger docks for order service

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # MicroservicesTestApp
-Test task 
+
+This is test task, required two microservices running in docker env.
+
+What was done:
+* added security generating custom jwt tokens for endpoint under protection
+* added kafka notifications for notifying user services that order is created
+* added unit tests for user service
+* added openapi docks for order controller
+* added registration and login endpoints under /auth/registration and /auth/login accordingly
+* added feign client to communicate between services
+
+
+What was not done:
+* caching with Hazelcast, lack of time to study some docks for integration
+* swagger do not seen from docker compos, never use it in docker env
+* do not added .sh .bash scripts for starting app, lack of time for testing
+
+How to:
+- cd to directory containing proj
+- cd /order & /user to build images
+- mvn clean install -P "docker" 
+- jib:dockerBuild
+- cd ..
+- docker compose up
+
--- order/src/main/java/org/microservices/order/controller/OrderController.java
+++ order/src/main/java/org/microservices/order/controller/OrderController.java
@@ -1,5 +1,9 @@
 package org.microservices.order.controller;
 
+ import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+import io.swagger.v3.oas.annotations.Operation;
 import org.microservices.order.model.Order;
 import org.microservices.order.service.OrderServices;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
+@Api(tags = {"Order Controller"})
 @RestController
 @RequestMapping(value = "/api/orders")
 public class OrderController {
@@ -18,27 +23,49 @@ public class OrderController {
         this.orderServices = orderServices;
     }
 
+    @Operation(summary = "Get all orders")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return all existing orders"),
+    })
     @GetMapping
     public ResponseEntity<List<Order>> getAll() { //Should return DTO instead of actual entity
         return ResponseEntity.ok().body(orderServices.getAll());
     }
 
+    @Operation(summary = "Get specific order by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return specific order"),
+    })
     @GetMapping("/{id}")
     public ResponseEntity<Order> getById(@PathVariable Long id) { //Should return DTO instead of actual entity
         return ResponseEntity.ok().body(orderServices.getById(id));
     }
 
-
+    @Operation(summary = "Save order")
+    @ApiResponses(value = {
+            @ApiResponse(code = 201, message = "Status of order creation"),
+            @ApiResponse(code = 400, message = "Status of order creation")
+    })
     @PostMapping
     public ResponseEntity<Order> save(@RequestBody Order order) {
         return ResponseEntity.status(201).body(orderServices.save(order));
     }
 
+    @Operation(summary = "Edit order")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Return updated order"),
+            @ApiResponse(code = 400, message = "Status of order update")
+    })
     @PutMapping("/{id}")
     public ResponseEntity<Order> update(@PathVariable Long id, @Valid @RequestBody Order order) {
         return ResponseEntity.ok().body(orderServices.update(id, order)); //Should get and return DTO instead of actual entity
     }
 
+    @Operation(summary = "Deleting order by id")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Status of order deletion"),
+            @ApiResponse(code = 400, message = "Status of order deletion")
+    })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         orderServices.deleteById(id);
